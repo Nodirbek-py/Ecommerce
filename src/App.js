3aa25a5ec7c81154a2b644c3d05859747e8b74eb
Asadbek13 <69428867+Asadbek13@users.noreply.github.com>
2021-07-09 19:46:17 +0500

    some changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react'
-import Home from "./views/home"
-import "./style.css"
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/navbar/navbar";
+import Footer from "./components/footer/footer";
+import Home from "./views/home";
+import "./style.css";
+
 const App = () => {
-  return <Home/>;
-}
+  return (
+    // <Router>
+    //   <Switch>
+    //     <Navbar />
+    //     <Route path="/">
+    //       <Home />
+    //     </Route>
+    //     <Footer />
+    //   </Switch>
+    // </Router>
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  );
+};
 export default App;
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,20 +1,17 @@
 import React from "react";
-import Navbar from "../components/navbar/navbar";
 import Latest from "../components/latest/latest";
 import Category from "../components/category/category";
 import Blog from "../components/blog/blog";
 import Newsletter from "../components/newsletter/newsletter";
-import Footer from "../components/footer/footer";
+
 const Home = () => {
-    return (
-        <div className="home">
-            <Navbar />
-            <Latest />
-            <Category />
-            <Blog />
-            <Newsletter />
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="home">
+      <Latest />
+      <Category />
+      <Blog />
+      <Newsletter />
+    </div>
+  );
 };
 export default Home;
